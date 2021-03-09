a1a2c528c3c30de964bb8ab67ea7f2a69d757668
hanjuren <juren528@gmail.com>
2021-03-09 14:47:05 +0900

    airbnb-clone

---
+++ airbnb/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Airbnb Clone Coding 🚀</h1>
+    
+    {/* Home */}
+        {/* Header */}
+
+        {/* Benner */}
+        
+        {/* Cards */}
+        
+        {/* Footer */}
+
+    {/* SerachPage */}
+        {/* ... */}
+    </div>
+  );
+}
+
+export default App;
